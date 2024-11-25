4385f9d5dfeff6f28e4139421773d2d64d2f65fb
Su Kwon <skwon2@c3r1p1.hive.fi>
2024-11-25 10:24:50 +0200

    additional rgb checkups

--- src/parsing_description/parsing_colors.c
+++ src/parsing_description/parsing_colors.c
@@ -6,7 +6,7 @@
 /*   By: skwon2 <skwon2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:35:02 by skwon2            #+#    #+#             */
-/*   Updated: 2024/11/21 17:31:02 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/11/25 10:22:41 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,22 @@ static void	set_rbg_color(t_caster *c, char *line, int *rgb)
 		c->textures->ceiling_color = color;
 }
 
+static int	check_digit(char *rgba_arr)
+{
+	int	i;
+
+	i = 0;
+	while (rgba_arr[i])
+	{
+		while (rgba_arr[i] && ft_isspace(rgba_arr[i]))
+			i++;
+		if (rgba_arr[i] && !ft_isdigit(rgba_arr[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	check_rgb_format(t_caster *c, char **rgba_arr, int rgb[3])
 {
 	int	i;
@@ -45,7 +61,7 @@ void	check_rgb_format(t_caster *c, char **rgba_arr, int rgb[3])
 	{
 		while (rgba_arr[i][j] && ft_isspace(rgba_arr[i][j]))
 			j++;
-		if (!ft_isdigit(rgba_arr[i][j]))
+		if (!check_digit(rgba_arr[i] + j))
 			exit_failure(c, "Invalid format of RGB color : Not a digit");
 		rgb_value = ft_atoi(rgba_arr[i] + j);
 		if (rgb_value < 0 || rgb_value > 255)
